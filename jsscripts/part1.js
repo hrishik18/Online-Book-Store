62e28d4ce698ab7a70027db16e0029776b0e5318
hrishik18 <hrishik.s@somaiya.edu>
2022-02-25 18:28:12 +0530

    javascript prac

---
+++ jsscripts/part1.js
@@ -0,0 +1,20 @@
+var button1 = document.createElement("button");
+button.innerHTML = "Q1 to show Date on clicking me";
+
+button1.addEventListener("click", function () {
+    alert("did something");
+});
+
+var button2 = document.createElement("button");
+button.innerHTML = "Q2 to show Date on clicking me";
+
+button2.addEventListener("click", function () {
+    alert("did something");
+});
+
+var button3 = document.createElement("button");
+button.innerHTML = "Q3 to show Date on clicking me";
+
+button3.addEventListener("click", function () {
+    alert("did something");
+});
